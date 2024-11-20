56472ee85792ce198c9025d0df9f9f733fb2630c
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-11-20 13:32:10 +0100

    Reestructured pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePageModule } from './pages/home/home.module';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },  {
+  },
+  {
     path: 'machine',
-    loadChildren: () => import('./machine/machine.module').then( m => m.MachinePageModule)
+    loadChildren: () => import('./pages/machine/machine.module').then( m => m.MachinePageModule)
   },
   {
     path: 'workout',
-    loadChildren: () => import('./workout/workout.module').then( m => m.WorkoutPageModule)
+    loadChildren: () => import('./pages/workout/workout.module').then( m => m.WorkoutPageModule)
   },
 
 ];

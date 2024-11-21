f35d3b55c0d2e372f4d1947c67718ed2c7fdeb89
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-11-21 14:39:04 +0100

    l

---
+++ src/app/core/services/interfaces/auth-mapping.interface.ts
@@ -0,0 +1,10 @@
+import { Userff } from './../../models/userff.model';
+import { SignInPayload, SignUpPayload, User } from "../../models/auth.model";
+
+export interface IAuthMapping{
+    signInPayload(payload:SignInPayload):any;
+    signUpPayload(payload:SignUpPayload):any;
+    signIn(response:any):User;
+    signUp(response:any):User;
+    me(response:any):User;
+  }

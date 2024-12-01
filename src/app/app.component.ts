9fec3b62908a41c84a53f45582835a31cf716d70
Miguel <falconmiguel.2004@gmail.com>
2024-12-01 14:51:51 +0100

    menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BaseAuthenticationService } from './core/services/impl/base-authentication.service';
+import { TranslationService } from './core/services/translate.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  currentLang: string;
+
+  constructor(
+    private translateService: TranslationService,
+    public authSvc: BaseAuthenticationService,
+    private router: Router
+  ) {
+    this.currentLang = this.translateService.getCurrentLanguage();
+  }
+
+  changeLanguage(lang: string) {
+    this.translateService.setLanguage(lang)
+    this.currentLang = lang;
+  }
+
+  logout() {
+    this.authSvc.signOut().subscribe(()=>{
+      this.router.navigate(['/login']);
+    });
+  }
 }

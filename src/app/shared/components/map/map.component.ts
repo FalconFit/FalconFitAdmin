29b887ec3c1a6f1951d908e4fc7871e044616e74
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-12-02 12:31:36 +0100

    small change to take the scss from map component

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent {
   center = { lat: 40.73061, lng: -73.935242 };

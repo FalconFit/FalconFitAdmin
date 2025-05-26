2e3eb8efe7ce315b2fe55e2068ef4e5d67c3a221
Miguel <falconmiguel.2004@gmail.com>
2025-05-27 00:10:17 +0200

    Tailwind added to Login page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.html",
+    "./src/**/*.html",
+    "./src/**/*.ts",
+    "./src/**/*.scss"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
